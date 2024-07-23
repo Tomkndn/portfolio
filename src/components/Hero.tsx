8f835877a57589331a606701368187c3738ff7e2
Tomkndn <ntamsoy@gmail.com>
2024-07-23 15:11:29 +0530

    added hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,13 @@
 "use client"
 import React from 'react'
 import { Image,Button } from "@nextui-org/react";
-// import { Button } from "./ui/moving-border";
+import { BorderBeam } from "./ui/Moving-border";
 
 function Hero() {
   return (
     <div className="flex justify-center items-center gap-20 mt-16 font-sans">
       <div>
-        <div className="text-lg my-3 ">
-          HELLO THERE, WELCOME TO MY SITE
-        </div>
+        <div className="text-lg my-3 ">HELLO THERE, WELCOME TO MY SITE</div>
         <p className="text-5xl text-wrap my-2 font-medium">I'm Kundan Tamsoy</p>
         <p className="text-6xl text-blue-800 font-bold my-2">
           A Full Stack Developer
@@ -17,16 +15,33 @@ function Hero() {
         <p className="text-5xl my-2 text-red-500 font-semibold">
           & Tech Enthusiast
         </p>
-        <Button>Resume</Button>
+        <Button
+          color="warning"
+          variant="shadow"
+          className="text-xl mt-5 h-10 w-36 rounded-xl bg-yellow-500"
+        >
+          Resume
+        </Button>
+        <Button variant="flat" className="text-xl mt-5 h-10 w-40 ms-10 ">
+          Hire Me 🎉
+          <BorderBeam
+            size={100}
+            borderWidth={1.3}
+            duration={3}
+            colorFrom="blue"
+            colorTo="red"
+          />
+        </Button>
       </div>
-      {/* <Button containerClassName="h-[600px] w-[500px]" borderClassName=""> */}
+      <div className="relative flex h-[608px] w-[508px] flex-col items-center justify-center rounded-large">
         <Image
           width={500}
           height={600}
           alt="NextUI hero Image"
           src="/myself.jpg"
         />
-      {/* </Button> */}
+        <BorderBeam size={300} borderWidth={4} duration={5} />
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,17 @@ const config: Config = {
             "offset-distance": "100%",
           },
         },
+        "shine-pulse": {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
       },
     },
   },
